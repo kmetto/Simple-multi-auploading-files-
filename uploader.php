55db19bb1412c5d1980e6f5777412fcb0230462f
Konstantin <met9127@gmail.com>
2016-06-27 10:55:05 +0300

    little fix

--- uploader.php
+++ uploader.php
@@ -17,24 +17,21 @@
 <?php
 require_once 'funct_uploader.php';
 
+$arr = filesArray($_FILES['file']);
 
-try {
-    $arr = filesArray($_FILES['file']);
-    foreach ($arr as $key => $file) {
-        if ($file['error'] == UPLOAD_ERR_OK) {
-            try {
-                checkSize($file['size'], 1000000000);
-                $ext = checkMimeType($file['type']);
-                $name = generataName($file['name'], $ext);
-                if (move_uploaded_file($file['tmp_name'], 'images/' . $name))
-                    echo "<span class='succ'>Файл " . $file['name'] . ' успешно загружен</span><br>';
-            } catch (Exception $e) {
-                echo "<span class='error'>Ошибка загрузки файла: " . $file['name'] . " " . $e->getMessage() . " " . "</span><br>";
-            }
+foreach ($arr as $key => $file) {
+    if ($file['error'] == UPLOAD_ERR_OK) {
+        try {
+            checkSize($file['size'], 1000000000);
+            $ext = checkMimeType($file['type']);
+            $name = generataName($file['name'], $ext);
+            if (move_uploaded_file($file['tmp_name'], 'images/' . $name))
+                echo "<span class='succ'>Файл " . $file['name'] . ' успешно загружен</span><br>';
+        } catch (Exception $e) {
+            echo "<span class='error'>Ошибка загрузки файла: " . $file['name'] . " " . $e->getMessage() . " " . "</span><br>";
         }
     }
-} catch (Exception $e) {
-    echo "<span class='error'>Ошибка загрузки файлов: " . $e->getMessage() . " " . "</span><br>";
-} ?>
+}
+?>
 </body>
 </html>
